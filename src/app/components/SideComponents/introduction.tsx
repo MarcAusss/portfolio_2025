3239cc263f7a20f6c98520ed21083bb15c1059eb
Marc Austin Bonagua <marcaustinbonagua@gmail.com>
2025-07-28 05:37:51 +0800

    sdsdasd

--- src/app/components/SideComponents/introduction.tsx
+++ src/app/components/SideComponents/introduction.tsx
@@ -3,13 +3,7 @@ export default function IntroDesc() {
     <div className="cursor-default">
       <h2 className="text-[2rem] pr-10">
         {"I'm a Web Developer specializing in ".split(" ").map((word, i) => (
-          <span
-            key={i}
-            className="word-slide"
-            style={{
-              animationDelay: `${i * 0.05 + 1.2 + 0}s`,
-            }}
-          >
+          <span key={i} className="word-slide" style={{ animationDelay: `${i * 0.05 + 1.2 + 0}s`,}}>
             {word}&nbsp;
           </span>
         ))}
@@ -18,13 +12,7 @@ export default function IntroDesc() {
         {"both front-end and back-end development."
           .split(" ")
           .map((word, i) => (
-            <span
-              key={i}
-              className="word-slide"
-              style={{
-                animationDelay: `${i * 0.05 + 1.5 + 0}s`,
-              }}
-            >
+            <span key={i} className="word-slide" style={{ animationDelay: `${i * 0.05 + 1.5 + 0}s`, }}>
               {word}&nbsp;
             </span>
           ))}
